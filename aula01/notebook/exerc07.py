135a764076119049dd8b96e99a2598e2f1f9cbfe
Lucas Ferreira <lucasgabrielrf.contato@gmail.com>
2024-08-20 14:46:54 -0300

    organization

--- aula01/notebook/exerc07.py
+++ aula01/notebook/exerc07.py
@@ -3,7 +3,9 @@
 import datetime
 
 dataNascimento = input("Insira sua data de nascimento (dd/mm/aaaa): ")
-dataNascimento = datetime.datetime.strptime(dataNascimento, "%d/%m/%Y")
+dataNascimento = datetime.datetime.strptime(dataNascimento, "%d/%m/%Y") 
 dataAtual = datetime.datetime.now()
 idade = dataAtual.year - dataNascimento.year
+if (dataAtual.month < dataNascimento.month):
+    idade -= 1
 print(f"Você tem {idade} anos.")
